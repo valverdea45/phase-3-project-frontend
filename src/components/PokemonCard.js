a6ad6801dbfbe095bb09aee8847929225a472103
Adrian <adrianvalverde45@gmail.com>
2023-05-17 16:28:02 -0400

    added no skill message

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -5,17 +5,24 @@ import AddSkill from "./AddSkill";
 function PokemonCard({ singlePokemon }) {
 
     const [skillShowing, setSkillShowing] = useState(false)
-    const [ addSkillShowing, setAddSkillShowing ] = useState(false)
-    const [ pokemonSkills, setPokemonSkills ] = useState(singlePokemon.pokemon_skills)
+    const [addSkillShowing, setAddSkillShowing] = useState(false)
+    const [pokemonSkills, setPokemonSkills] = useState(singlePokemon.pokemon_skills)
+    const [noSkills, setNoSkills] = useState(false)
 
     function handleClick() {
-        setSkillShowing((skillShowing) => !skillShowing)
-        setAddSkillShowing(false)
+        if (pokemonSkills.length === 0) {
+            setNoSkills((noSkills) => !noSkills)
+            setAddSkillShowing(false)
+        } else {
+            setSkillShowing((skillShowing) => !skillShowing)
+            setAddSkillShowing(false)
+        }
     }
 
     function handleNewSkillClick() {
         setAddSkillShowing((addSkillShowing) => !addSkillShowing)
         setSkillShowing(false)
+        setNoSkills(false)
     }
 
     const pokemonSkillsDisplayed = pokemonSkills.map((skill) => {
@@ -27,20 +34,26 @@ function PokemonCard({ singlePokemon }) {
     }
 
     return (
-       <div>
-        <h3>pokemon name: {singlePokemon.name}</h3>
-        <p>pokemon level: {singlePokemon.level}</p>
-        <img src={singlePokemon.sprite} alt="pokemon"/>
-        <button onClick={handleClick}>Skills</button>
-        <button onClick={handleNewSkillClick}>Add Skills</button>
-        {skillShowing ? (
-            pokemonSkillsDisplayed
-        ) : null}
-        {addSkillShowing ? (
-            <AddSkill onAddSkill={onAddSkill} />
-        ) : null}
-        
-       </div>
+        <div>
+            <h3>pokemon name: {singlePokemon.name}</h3>
+            <p>pokemon level: {singlePokemon.level}</p>
+            <img src={singlePokemon.sprite} alt="pokemon" />
+            <button onClick={handleClick}>Skills</button>
+            <button onClick={handleNewSkillClick}>Add Skills</button>
+            {skillShowing ? (
+                pokemonSkillsDisplayed
+            ) : null}
+            {noSkills ? (
+                <div>
+                    <p>This pokemon has no skills</p>
+                    <p>Try adding some!</p>
+                </div>
+            ) : null}
+            {addSkillShowing ? (
+                <AddSkill onAddSkill={onAddSkill} />
+            ) : null}
+
+        </div>
     )
 }
 
