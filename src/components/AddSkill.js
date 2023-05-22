d3cb211c08dff34c3e5a234258ab2cf768ff98c3
Adrian <adrianvalverde45@gmail.com>
2023-05-22 16:34:09 -0400

    added invalid input

--- src/components/AddSkill.js
+++ src/components/AddSkill.js
@@ -5,47 +5,60 @@ function AddSkill({ onAddSkill, pokemonId }) {
     const [name, setName] = useState("")
     const [powerPoints, setPowerPoints] = useState(0)
     const [description, setDescription] = useState("")
+    const [isInvalidInput, setIsInvalidInput] = useState(false)
 
     function handleSubmit(e) {
-
-        console.log("submitted")
-
         e.preventDefault()
 
-        const objToBeSent = {
-            name: name,
-            description: description,
-            power_points: powerPoints,
-            pokemonId: pokemonId
+        if (name === "" || powerPoints === 0 || description === "") {
+            setIsInvalidInput(true)
+        } else {
+            console.log("submitted")
+
+            
+
+            const objToBeSent = {
+                name: name,
+                description: description,
+                power_points: powerPoints,
+                pokemonId: pokemonId
+            }
+
+            fetch("http://localhost:9292/skills", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(objToBeSent)
+            })
+                .then((data) => data.json())
+                .then((newSkills) => {
+                    onAddSkill(newSkills)
+                })
+
+
+            setName("")
+            setPowerPoints(0)
+            setDescription("")
         }
 
-        fetch("http://localhost:9292/skills", {
-            method: "POST",
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(objToBeSent)
-        })
-        .then((data) => data.json())
-        .then((newSkills) => {
-           onAddSkill(newSkills) 
-        })
 
-
-        setName("")
-        setPowerPoints(0)
-        setDescription("")
     }
 
     return (
         <div>
-            <p>you can add skills here!</p>
             <form onSubmit={handleSubmit}>
-                <input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder={"Skill name"}/>
-                <input onChange={(e) => setPowerPoints(e.target.value)} value={powerPoints} type="number" placeholder="Power Points"/>
-                <input onChange={(e) => setDescription(e.target.value)} value={description} type="text" placeholder="Description"/>
+                <label> Name: </label>
+                <input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder={"Skill name"} />
+                <label> Power Points: </label>
+                <input onChange={(e) => setPowerPoints(e.target.value)} value={powerPoints} type="number" placeholder="Power Points" />
+                <label> Description: </label>
+                <input onChange={(e) => setDescription(e.target.value)} value={description} type="text" placeholder="Description" />
                 <button type="submit">Add Skill</button>
             </form>
+            {isInvalidInput ? (
+                <p>oops - please fill out all the fields.</p>
+            ) : null}
         </div>
     )
 }
